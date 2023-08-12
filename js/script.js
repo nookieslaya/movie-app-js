f4eff1b9616e20ae35be48a391b4878338d603d0
Radek <radek.dumin@gmail.com>
2023-08-12 11:15:53 +0200

    Popular movies

--- js/script.js
+++ js/script.js
@@ -1,5 +1,50 @@
 const url = '3ec38e84a0b560dd9b09c620f3b8fefc'
 
+//Display movies
+async function displayPopularMovies() {
+	const { results } = await fetchAPIData('movie/popular')
+	console.log(results)
+
+	results.forEach(movie => {
+		const div = document.createElement('div')
+		div.classList.add('card')
+		div.innerHTML = `
+        <a href="movie-details.html?id=${movie.id}">
+           ${
+							movie.poster_path
+								? ` <img
+           src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+           class="card-img-top"
+           alt="Movie Title"
+         />`
+								: ` <img
+         src="images/no-image.jpg"
+         class="card-img-top"
+         alt="${movie.title}"
+       />`
+						}
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release:${movie.release_date}</small>
+            </p>
+          </div>
+        `
+		document.querySelector('#popular-movies').appendChild(div)
+	})
+}
+
+async function fetchAPIData(endpoint) {
+	const API_KEY = '3ec38e84a0b560dd9b09c620f3b8fefc'
+	const API_URL = 'https://api.themoviedb.org/3/'
+
+	const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`)
+	const data = await response.json()
+	console.log(data)
+	return data
+}
+
 const global = {
 	currentPage: window.location.pathname,
 }
@@ -16,7 +61,7 @@ function init() {
 	switch (global.currentPage) {
 		case '/':
 		case '/index.html':
-			console.log('Home')
+			displayPopularMovies()
 			break
 		case '/shows.html':
 			console.log('Shows')
